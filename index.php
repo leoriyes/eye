f6ffbaae9c70a6a306d88a66c8c3086c4f3d5bea
leoriyes <leoriyes@mail.ru>
2020-12-18 20:36:19 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Version for the visually impaired</title>
+
+    <!--<script src="eye/special.js"></script>-->
+    <link rel="stylesheet" href="/css/bvi.min.css" type="text/css">
+</head>
+<body>
+    <a href="#" class="bvi-open">Switch to visually impaired mode</a>
+
+
+    <h1>Title</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat ipsum excepturi illo accusamus amet repellat at aliquam aspernatur? Sapiente quisquam nulla quaerat dignissimos repellat ipsa esse similique tempora provident non laboriosam illum, blanditiis repellendus facere. Sed, quam beatae! Quae corporis facilis quis ab saepe dolorem? Non voluptatem porro ducimus ab id officiis, est nam ea sint tempora. Modi illum, pariatur numquam ea consequuntur voluptatum quisquam exercitationem animi, earum tenetur autem tempore dolor? Ad ut ratione impedit magni quos voluptatem perspiciatis totam, deserunt sed! Natus tempore pariatur quam deserunt labore, quae commodi atque unde provident amet eum impedit, quis dicta modi!</p>
+    <a href="#">Click <</a>
+    <h2>Title h2</h2>
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="/js/js.cookie.js"></script>
+<script src="/js/bvi-init.js"></script>
+<script src="/js/bvi.min.js"></script>
+</body>
+</html>
